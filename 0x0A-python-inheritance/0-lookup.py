612984d4178a6fb1f9cfb3a0193ba05541c9a8e6
Royalty <akannie33@gmail.com>
2023-02-06 14:36:13 +0100

    Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" function that returns the list of available attributes of an object
+"""
+
+def lookup(obj):
+    """ function: lookup
+    Returns a list of object
+    """
+
+    return dir(obj)
+
