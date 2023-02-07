3b78e1baa85ddf40025ae5902422fa6413b9b172
Royalty <akannie33@gmail.com>
2023-02-07 16:13:59 +0100

    code fixed

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,8 +4,7 @@ function that writes to a file
 """
 
 def write_file(filename="", text=""):
-    """writes a text to a text file
-    Returns none
+    """module write_file 
     """
-    with open(filename, mode="w", encoding="utf-8") as myFile:
-        myFile.write(text)
+    with open(filename, mode="w") as myFile:
+        return myFile.write(text)
