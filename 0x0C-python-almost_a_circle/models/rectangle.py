01b09875e9aeab657873845ea9c9557419f8f9a1
Akanni Emmanuel <102362366+Akanni5@users.noreply.github.com>
2023-02-15 13:53:45 +0100

    rectangle.py update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -27,3 +27,48 @@ class Rectangle(Base):
         '''
         self.setter_validation("width", value)
         self.__width = value
+
+    @property
+    def height(self):
+        '''
+            __height getter
+        '''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("height", value)
+        self.__height = value
+
+    @property
+    def x(self):
+        '''
+            __x getter
+        '''
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        '''
+            set private attribute
+        '''
+        self.setter_validation("x", value)
+        self.__x = value
+
+	@property
+    def y(self):
+        '''
+            __y getter
+        '''
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        '''
+            set private attribute
+        '''
+        self.setter_validation("y", value)
+        self.__y = value
