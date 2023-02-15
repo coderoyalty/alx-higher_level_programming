13d0820ab3dee14296186b6f93c4f5246376bc63
Royalty <akannie33@gmail.com>
2023-02-15 13:47:46 +0100

    Rectangle class

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    Rectangle Class
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        '''
+            __width getter
+        '''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("width", value)
+        self.__width = value
