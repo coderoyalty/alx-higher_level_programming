88eb8ed4f993474d577992ad52b5614ebc94b925
Akanni Emmanuel <102362366+Akanni5@users.noreply.github.com>
2023-02-17 12:26:59 +0100

    forgot to return value

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -50,4 +50,4 @@ class Base:
         if list_dictionaries is None:
             return '[]'
         else:
-            json.dumps(list_dictionaries)
+            return json.dumps(list_dictionaries)
