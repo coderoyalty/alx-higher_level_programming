7d00e2cfd03283554db5bc2e78003a861ed1a1e9
Royalty <akannie33@gmail.com>
2023-04-11 17:28:13 +0100

    js task 6 solution

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const lines = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+for (let index = 0; index < lines.length; index += 1) {
+  console.log(lines[index]);
+}
