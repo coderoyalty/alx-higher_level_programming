33ee8aac13e56475fe78d4dbf85c82572f2839f7
Royalty <akannie33@gmail.com>
2023-04-12 17:53:24 +0100

    advance task 2 solution

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,10 @@
+#!usr/bin/node
+
+// this script concats 2 files.
+
+const argv = process.argv;
+const fs = require('fs');
+const firstContent = fs.readFileSync(`./${argv[2]}`);
+const secondContent = fs.readFileSync(`./${argv[3]}`);
+const content = firstContent + secondContent;
+fs.writeFileSync(`./${argv[4]}`, content);
