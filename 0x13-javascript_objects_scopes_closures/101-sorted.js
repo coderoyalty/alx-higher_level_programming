7feb0a2921727f41641f862e3b3e73bd4e33abf8
Royalty <akannie33@gmail.com>
2023-04-12 17:39:58 +0100

    codestyle error

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -4,13 +4,12 @@ const dict = require('./101-data').dict;
 
 const newDict = {};
 
-for (let key in dict) {
-    const val = dict[key];
-    if (newDict[val] === undefined) {
-        newDict[val] = []
-    }
-    newDict[val].push(key)
+for (const key in dict) {
+  const val = dict[key];
+  if (newDict[val] === undefined) {
+    newDict[val] = [];
+  }
+  newDict[val].push(key);
 }
 
 console.log(newDict);
-
