fd7f246fc4c8f2660b24f783db3b8d7b4f1b3d61
Akanni Emmanuel <akannie33@gmail.com>
2023-04-14 16:10:05 +0100

    my filter states

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+    this script list all states from
+    a database.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    ''' the database user '''
+    db_user = sys.argv[1]
+    ''' the database password '''
+    db_password = sys.argv[2]
+    ''' the database name '''
+    db_name = sys.argv[3]
+
+    my_filter = sys.argv[4]
+
+    ''' connect to the database '''
+    db = MySQLdb.connect(host="localhost", user=db_user,
+                         passwd=db_password, db=db_name)
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE '{:s}'"
+                   "ORDER BY id ASC".format(my_filter))
+
+    for output in cursor:
+        print(output)
+    cursor.close()
+    db.close()
