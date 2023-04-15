54f1e974ab01e21a6c9755c055296dde5471972b
Akanni Emmanuel <akannie33@gmail.com>
2023-04-15 16:52:56 +0100

    get state

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+''' script to get a state
+'''
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+Session = sessionmaker()
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(
+                               sys.argv[1],
+                               sys.argv[2],
+                               sys.argv[3]
+                           ))
+    Base.metadata.create_all(engine)
+    Session.configure(bind=engine)
+    session = Session()
+    state = session.query(State)\
+        .filter(State.name == sys.argv[4]).all()
+    if len(state) != 0:
+        print(state[0].id)
+    else:
+        print("Nothing found")
+ 
