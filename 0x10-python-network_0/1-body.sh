8f59960d5c6362b2b309715901ae23ee837d3f93
Akanni Emmanuel <akannie33@gmail.com>
2023-04-27 12:06:50 +0100

    bash script must be exactly 3 lines long

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# sends a GET request to a URL and display only body of
-# a 200 status code response
-
-
-response=$(curl -s -w "\n%{http_code}\n" "$1")
-status_code=$(echo "$response" | tail -n 1)
-
-if [ "$status_code" -eq 200 ]; then
-	echo "$response" | head -n -2
-fi
+# sends a GET request to a URL
+curl -sL $1
