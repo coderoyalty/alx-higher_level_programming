dd8cdc41a1ffff4c11d6b6570aeccfb54d80750d
Akanni Emmanuel <akannie33@gmail.com>
2023-04-27 17:33:22 +0100

    script update

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#displays only the status code of the response from a URL
-curl -s -I $1 | grep "HTTP" | cut -d " " -f2
+# displays only the status code of the response
+curl -s -I "$1" | grep "HTTP" | cut -d " " -f2
