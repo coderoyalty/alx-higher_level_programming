da44e6712f5b906808db7b1195fede8ab692d1ba
Akanni Emmanuel <akannie33@gmail.com>
2023-04-27 18:06:29 +0100

    script to fetch a url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+fetches a url using urllib
+"""
+import urllib.request
+
+if __name__ == '__main__':
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as resp:
+        content = resp.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
