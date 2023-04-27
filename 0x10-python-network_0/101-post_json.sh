290797e944478f4bc70c16e1d0a5347d74576f2f
Akanni Emmanuel <akannie33@gmail.com>
2023-04-27 17:46:53 +0100

    bug fix

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a post request with the contents of a file
-curl -s -H "Content-Type: application/json" -X POST -d "@$1" "$2" 
+curl -s -H "Content-Type: application/json" -X POST -d "@$2" "$1"
