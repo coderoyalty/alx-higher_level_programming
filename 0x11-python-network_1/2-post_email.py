786b6ed1c5697eacddd44c7bd8c62d2cd17aff25
Akanni Emmanuel <akannie33@gmail.com>
2023-04-28 22:06:57 +0100

    post an email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+script that sends a post request to the passed URL
+with the email as a parameter, and displays the body
+of the response
+'''
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    value = sys.argv[2]
+    data = {"email": value}
+
+    data = urllib.parse.urlencode(data)
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as res:
+        print(res.read().decode())
