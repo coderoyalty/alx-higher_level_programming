a68cff494a26c7dfc6303cc2907c2e2d17043cf4
Akanni Emmanuel <akannie33@gmail.com>
2023-04-28 22:19:03 +0100

    bug fixes

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -14,6 +14,6 @@ if __name__ == '__main__':
     data = {"email": value}
 
     data = urllib.parse.urlencode(data).encode('ascii')
-    req = urllib.request.Request(url)
+    req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as res:
         print(res.read().decode('utf-8'))
