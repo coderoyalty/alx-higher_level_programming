8ab46db761d17d6cd7d765bae4110ccb2530ba77
Akanni Emmanuel <akannie33@gmail.com>
2023-04-28 20:45:22 +0100

    using get to access dictionary value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,12 +4,14 @@ script to send a request to a URL and displays the value
 of the X-Request-Id variable in the header of the response
 from the URL.
 '''
+
 import urllib.request
 import sys
 
 
 if __name__ == '__main__':
     url = sys.argv[1]
+
     with urllib.request.urlopen(url) as response:
         headers = response.info()
-        print(headers['X-Request-Id'])
+        print(headers.get('X-Request-Id'))
