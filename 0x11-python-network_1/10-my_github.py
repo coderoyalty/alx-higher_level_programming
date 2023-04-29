5940e444379f0200e6a486fb4371ebf0e2ac7dad
Akanni Emmanuel <akannie33@gmail.com>
2023-04-29 21:50:55 +0100

    get a github username id

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,6 +9,6 @@ import sys
 if __name__ == '__main__':
     user = sys.argv[1]
     pat = sys.argv[2]
-    url = 'https://api.github.com/{}'.format(user)
+    url = 'https://api.github.com/users/{}'.format(user)
     res = requests.get(url, auth=(user, pat))
     print(res.json().get('id'))
