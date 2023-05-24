04dab6604ef786c6792df407cbab0df9ccb28396
Akanni Emmanuel <akannie33@gmail.com>
2023-05-24 11:15:39 +0100

    content of a webpage

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const fs = require('fs')
+const request = require('request')
+const argv = process.argv;
+const url = argv[2];
+const filePath = argv[3];
+
+request.get(url, (err, response, body) => {
+	if (err) {
+		console.error(err);
+		return;
+	}
+	if (response.statusCode != 200) {
+		return;
+	}
+
+	fs.writeFile(filePath, body, 'utf-8', (err) => {
+		if (err) {
+			console.error(err);
+			return;
+		}
+	})
+});
