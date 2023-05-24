c1863de50e78f83cda3bafe3c277d43e9fce04ef
Akanni Emmanuel <akannie33@gmail.com>
2023-05-24 10:37:20 +0100

    star wars character

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+const request = require('request');
+const argv = process.argv;
+const url = argv[2];
+const char_id = "18";
+
+function handler(err, response, body) {
+	if (err) {
+		console.error(err);
+		return;
+	}
+	if (response.statusCode != 200) {
+		return;
+	}
+	const data = JSON.parse(body);
+	const results = data["results"];
+	let total = 0;
+	const resultHandler = (result) => {
+		const characters = result["characters"];
+		characters.forEach((character) => {
+			const tokens = character.split("/");
+			const index = tokens.length - 2;
+			if (tokens[index] == char_id) {
+				total++;
+			}
+			return;
+		})
+		return;
+	}
+	results.forEach(resultHandler);
+	console.log(total);
+}
+
+request.get(`${url}`, handler);
